72c516e331eed9e4cd9b2ee960eb5a98a5bf251a
John Hinch <johnhnch864@gmail.com>
2023-02-17 12:46:18 +0000

    LoopsB

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -54,6 +54,61 @@ public class Loops extends PApplet {
 				}
 
 				break;
+			case 2:
+				stroke(255);
+				float border = 50.0f;
+				textAlign(CENTER,CENTER);
+				textSize(15);
+		
+				int count = 10;
+				float gap = (width - (border * 2.0f)) / (float) count;
+				for(int i = -5 ; i <= 5 ; i ++)
+				{
+					
+					float x = border + (gap * (i + 5));
+					line(x, border, x, height - border);
+					line(border, x, width - border, x);
+					fill(255);
+					text(i,x,(border*0.5f));
+					text(i,(border*0.5f),x);
+				}
+				break;
+				case 3:
+					background(0);
+					stroke(255,255,255);
+					float circleX=width/2;
+					float circleY=height/2;
+					float radius =200;
+					int points = (int)map(mouseX,1,width,5,20);
+					int sides = points*2;
+					float pointX=circleX;
+					float pointY=circleY;
+					for(int i=0;i<=sides;i++)
+					{
+						float r=(i%2==0)? radius:radius/2;
+						float theta = map(i,0,sides,0,TWO_PI);
+						float x=circleX+sin(theta)*r;
+						float y = circleY+cos(theta)*r;
+						line(pointX,pointY,x,y);
+						pointX=x;
+						pointY=y;
+				
+
+					}
+				break;
+				case 4:
+				background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width / (float) squares;
+				for (int i = 0; i < squares; i++) {
+					noStroke();
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					float x = map(i, 0, squares, 0, width);
+					rect(x, x, h, h);
+					rect((width - h) - x, x, h, h);
+				}
+				break;
+
 			default:
 				break;
 		}
