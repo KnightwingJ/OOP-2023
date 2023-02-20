3c87d022db21e238adb4039a2b90158f54bce384
John Hinch <johnhnch864@gmail.com>
2023-02-20 12:59:15 +0000

    Audio

--- java/src/ie/tudublin/audio1.java
+++ java/src/ie/tudublin/audio1.java
@@ -40,22 +40,26 @@ public class audio1 extends PApplet
 		background(0);
 		stroke(255);
 
+		float total=0;
 		float half=height/2;
 		float cGap=255/(float)ab.size();
 		for(int i=0; i<ab.size(); i++)
 		{
+			total+=abs(ab.get(i));
 			stroke(cGap*i,255,255);
 			line(i,half,i,half+ab.get(i)*half);
 
 		}
-		float average=0;
-		for(int i=0;i<ab.size();i++)
-		{
-			average=+ab.get(i);
-			abs(average);
-			stroke(cGap*i,255,255);
-			circle(half, width/2, average);
-		}
+		float average=total/(float)ab.size();
+		float r= average *200;
+
+		lerpedR=lerp(lerpedR,r,0.1f);
+
+		circle(100,200,lerpedR);
+
+		//lerp(a,b,t);
+		//map
 		
 	}
+	float lerpedR=0;
 }
