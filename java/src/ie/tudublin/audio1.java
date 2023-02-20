868dfba341a038674275520d305562a4f7d43e9a
John Hinch <johnhnch864@gmail.com>
2023-02-20 12:21:13 +0000

    Audio 1

--- java/src/ie/tudublin/audio1.java
+++ java/src/ie/tudublin/audio1.java
@@ -1,6 +1,9 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 
 public class audio1 extends PApplet
