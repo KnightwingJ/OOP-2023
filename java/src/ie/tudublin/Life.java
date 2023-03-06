e369749cfd064642821cce65fa51b4ce78649be7
John Hinch <johnhnch864@gmail.com>
2023-03-06 12:54:46 +0000

    Life Bug

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -2,26 +2,25 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Life extends PApplet
-{
+public class Life extends PApplet {
+	LifeBoard board;
 
-	public void settings()
-	{
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
+		board = new LifeBoard(50, this);
+		board.randomise();
 
-		
-		
 	}
 
-	
-	
-	public void draw()
-	{	
-		
+	public void draw() {
+		background(0);
+		board.render();
+		board.applyRules();
+
 	}
 }
